e5b1dd83191d47fb0da2e5d8ab93a3d639af9d72
Thomas <thomas.curti@epita.fr>
2020-09-19 10:49:52 +0200

    Backend: Init: Add init of Backend
    Add mvnw cmd to make sure that everyone could start it

---
+++ MindMapBack/MindMapBack/src/main/java/com/epita/pfee/MindMapBack/MindMapBackApplication.java
@@ -0,0 +1,13 @@
+package com.epita.pfee.MindMapBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindMapBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindMapBackApplication.class, args);
+	}
+
+}
---
+++ MindMapBack/MindMapBack/src/test/java/com/epita/pfee/MindMapBack/MindMapBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epita.pfee.MindMapBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMapBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
