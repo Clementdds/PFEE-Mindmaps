e879d821fa9276baca7ee8a1b15e628ef8d3043f
Vincent Masson <vincent1.masson@epita.fr>
2020-10-16 17:06:15 +0200

    Backend: TokenManager: create GetIdFromToken

--- MindMapBack/MindMapBack/utils/utils-jwt/src/main/java/utils/TokenManager.java
+++ MindMapBack/MindMapBack/utils/utils-jwt/src/main/java/utils/TokenManager.java
@@ -3,6 +3,9 @@ package utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 
 public class TokenManager {
     private static String Issuer = "PFEE-Mindmaps";
@@ -22,5 +25,20 @@ public class TokenManager {
             //Invalid Signing configuration / Couldn't convert Claims.
         }
         return token;
+    }
+
+    public static Integer GetIdFromToken(String token)
+    {
+        DecodedJWT jwt = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(Secret);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(Issuer)
+                    .build(); //Reusable verifier instance
+            jwt = verifier.verify(token);
+        } catch (JWTVerificationException exception){
+            return -1;
+        }
+        return jwt.getClaim("userId").asInt();
     }
 }
