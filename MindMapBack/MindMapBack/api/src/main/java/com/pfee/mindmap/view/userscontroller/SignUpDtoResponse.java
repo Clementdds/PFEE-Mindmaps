c629b2c64602c4ad829943e5770fb5c9c1b8c8b1
Vincent Masson <vincent1.masson@epita.fr>
2020-10-16 16:23:10 +0200

    Backend: api: make ctor attributes final

--- MindMapBack/MindMapBack/api/src/main/java/com/pfee/mindmap/view/userscontroller/SignUpDtoResponse.java
+++ MindMapBack/MindMapBack/api/src/main/java/com/pfee/mindmap/view/userscontroller/SignUpDtoResponse.java
@@ -4,7 +4,7 @@ public class SignUpDtoResponse {
     public final String token;
     public final String error;
 
-    public SignUpDtoResponse(String token, String error) {
+    public SignUpDtoResponse(final String token, final String error) {
         this.token = token;
         this.error = error;
     }
