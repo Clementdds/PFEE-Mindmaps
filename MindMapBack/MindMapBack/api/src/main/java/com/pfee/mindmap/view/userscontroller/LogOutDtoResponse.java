de916f5a3df8e30973fdcf3b2dfbb73ce31a18d0
Vincent Masson <vincent1.masson@epita.fr>
2020-10-16 17:10:30 +0200

    Backend: api: logout dto type

---
+++ MindMapBack/MindMapBack/api/src/main/java/com/pfee/mindmap/view/userscontroller/LogOutDtoResponse.java
@@ -0,0 +1,9 @@
+package com.pfee.mindmap.view.userscontroller;
+
+public class LogOutDtoResponse {
+    public final String error;
+
+    public LogOutDtoResponse(String error) {
+        this.error = error;
+    }
+}
