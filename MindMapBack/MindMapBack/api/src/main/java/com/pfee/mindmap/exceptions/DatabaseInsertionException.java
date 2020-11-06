468f7d9f057902b05ad5d0f137b47263ee1cba69
Vincent Masson <vincent1.masson@epita.fr>
2020-11-06 15:03:36 +0100

    Backend: DatabaseInsertionException: updated to 502

--- MindMapBack/MindMapBack/api/src/main/java/com/pfee/mindmap/exceptions/DatabaseInsertionException.java
+++ MindMapBack/MindMapBack/api/src/main/java/com/pfee/mindmap/exceptions/DatabaseInsertionException.java
@@ -3,5 +3,5 @@ package com.pfee.mindmap.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Error during database insertion")
+@ResponseStatus(value = HttpStatus.BAD_GATEWAY, reason = "Error during database insertion")
 public class DatabaseInsertionException extends RuntimeException {}
