f08d87414a1260dc89a42262a17c9a715f78c5f3
Jeremie Piro <jeremie.piro@epita.fr>
2020-11-12 12:39:50 +0100

    Frontend: Login Page

--- WebSite/src/Layout/LoginPage.js
+++ WebSite/src/Layout/LoginPage.js
@@ -49,43 +49,67 @@ const LoginPage = ({isSignedIn, error, dispatch}) => {
             {
                 isSignedIn && <Redirect to="/"/>
             }
-
-            <div>
-                <div>
-                    <h2>
-                        Sign in to your account !
-                    </h2>
-                    <Link to={"/sign"}>
-                        Go to sign up
-                    </Link>
-                </div>
-                <div>
-                    <div>
-                        <div>
+            <div class="form-group row formulaire " >
+                <div class="col"/>
+                <div class="col">
+                    <center>
+                        <h2>
+                            Connexion
+                        </h2>
+                    </center>
+                    <label class="row formulaire">
+                        <div class="col-3"> 
+                            <center>
+                            <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-envelope-fill text-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+                            </svg>
+                            </center>
+                        </div>
+                        <div class="col">
                             <input aria-label="Email address" type="email" required
                                    placeholder="Email address"
                                    onChange={handleEmailOnchange}
+                                   class="form-control"
                             />
                         </div>
-                        <div>
+                    </label>
+                    <label class="row">
+                        <div class="col-3">
+                            <center>
+                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-lock-fill text-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2.5 9a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-7a2 2 0 0 1-2-2V9z"/>
+                                    <path fill-rule="evenodd" d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>
+                                </svg>
+                            </center>
+                        </div>
+                        <div class="col">
                             <input aria-label="Password" type="password" required
                                    placeholder="Password"
                                    onChange={handlePasswordOnchange}
+                                   class="form-control"
                             />
                         </div>
-                    </div>
-
-                    <div>
-                        <button type="submit"
+                    </label>
+                    <div class="row">
+                        <div class="col-3"/>
+                        <div class="col-4 marginTop2p">
+                            <Link to={"/sign"}>
+                                Créer un compte
+                            </Link>
+                        </div>
+                        <div class="col"/>
+                        <div class="col-3">
+                            <button type="submit" class="btn btn-primary"
                                 disabled={email === '' || password === ''}
                                 onClick={handleAuth}
-                        >
-                            Sign in
-                        </button>
+                                >
+                                Connexion
+                            </button>
+                        </div>
                     </div>
                 </div>
+                <div class="col"/>
             </div>
-
             {error && <strong>{error}</strong>}
 
         </div>
