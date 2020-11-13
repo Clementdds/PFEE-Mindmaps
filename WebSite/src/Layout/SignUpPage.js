0a4cfc9d439cc5995e065a68290e15bc82772727
Jeremie Piro <jeremie.piro@epita.fr>
2020-11-13 09:56:09 +0100

    Frontend: SignIn Page

--- WebSite/src/Layout/SignUpPage.js
+++ WebSite/src/Layout/SignUpPage.js
@@ -59,48 +59,84 @@ const SignUpPage = ({error, dispatch}) => {
 
     return (
         <div>
-            <div>
-                <div>
-                    <h2>
-                        Sign Up !
-                    </h2>
-                    <Link to={"/login"}>
-                        Go to login
-                    </Link>
-                </div>
-                <div>
-                    <div>
-                        <div>
+              <div class="form-group row formulaire " >
+                <div class="col"/>
+                <div class="col">
+                    <center>
+                        <h2>
+                            Créer votre compte
+                        </h2>
+                    </center>
+                    <label class="row formulaire">
+                        <div class="col-3"> 
+                            <center>
+                            <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-envelope-fill text-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+                            </svg>
+                            </center>
+                        </div>
+                        <div class="col">
                             <input aria-label="Email address" type="email" required
                                    placeholder="Email address"
                                    onChange={handleEmailOnchange}
+                                   class="form-control"
                             />
                         </div>
-                        <div>
+                    </label>
+                    <label class="row">
+                        <div class="col-3">
+                            <center>
+                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-lock-fill text-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2.5 9a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-7a2 2 0 0 1-2-2V9z"/>
+                                    <path fill-rule="evenodd" d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>
+                                </svg>
+                            </center>
+                        </div>
+                        <div class="col">
                             <input aria-label="Password" type="password" required
                                    placeholder="Password"
                                    onChange={handlePasswordOnchange}
+                                   class="form-control"
                             />
                         </div>
-                        <div>
+                    </label>
+                    <label class="row">
+                        <div class="col-3">
+                            <center>
+                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-lock-fill text-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2.5 9a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-7a2 2 0 0 1-2-2V9z"/>
+                                    <path fill-rule="evenodd" d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>
+                                </svg>
+                            </center>
+                        </div>
+                        <div class="col">
                             <input aria-label="Password" type="password" required
                                    placeholder="Verify Password"
                                    onChange={handleVerifyPasswordOnchange}
+                                   class="form-control"
                             />
                         </div>
-                    </div>
+                    </label>
+                    <div class="row">
+                        <div class="col-3"/>
+                        <div class="col-4 text-left">
+                            <Link to={"/login"}>
+                                Se connecter avec un compte existant
+                                </Link>
 
-                    <div>
-                        <button type="submit"
+                        </div>
+                        <div class="col text-right">
+                            <button type="submit"  class="btn btn-primary"
                                 disabled={email === '' || password === '' || verifyPassword === '' || verifyPassword !== password}
                                 onClick={handleAuth}
-                        >
-                            Sign in
-                        </button>
+                            >
+                            Inscription
+                            </button>
+                        </div>
                     </div>
                 </div>
+            <div class="col"/>
             </div>
-
             {error && <strong>{error}</strong>}
 
         </div>
