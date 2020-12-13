c90a4dbe835a024164e36e2d5cbee9cc534f0c76
Thomas <thomas.curti@epita.fr>
2020-12-13 14:29:56 +0100

    terraform: Remove swagger for prod

--- Terraform/run_site.sh
+++ Terraform/run_site.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone git@github.com:Clementdds/PFEE-Mindmaps.git
+git clone https://github.com/Clementdds/PFEE-Mindmaps.git
 sudo apt-get update -y
 sudo apt-get install -y maven openjdk-14-jdk nodejs npm
-cd MindMapBack/MindMapBack
-mvn compile
-nohup mvn exec:java -Dexec.mainClass=com.pfee.mindmap.MindmapApplication & > /tmp/log_back.txt
+cd PFEE-Mindmaps/MindMapBack/MindMapBack
+mvn package -e > /tmp/mvn_package_log.txt
+cp webapp/target/webapp-0.0.1-SNAPSHOT.jar backend.jar
+nohup java -jar backend.jar > /tmp/log_back.txt &
+touch BackendStarted /tmp
 cd ../../WebSite
 npm install
-nohup npm run & > /tmp/log_website.txt
+nohup npm run start > /tmp/log_website.txt &
+touch WebsiteStarted /tmp
